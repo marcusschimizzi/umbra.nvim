1e221dbe0c26a6830118ef9bbbcabe5a5839a38b
Marcus Schimizzi <marcus@schimizzi.io>
2025-03-02 16:43:47 -0800

    add initial theme configuration

---
+++ lua/umbra/colors.lua
@@ -0,0 +1,36 @@
+local colors = {
+  -- Core backgrounds
+  umbra = "#121218",
+  penumbra = "#1e1e26",
+  antumbra = "#2a2a36",
+
+  -- Highlight states
+  eclipse = "#1a1a23",
+  occult = "#252532",
+  transit = "#313142",
+
+  -- Text colors
+  corona = "#e8e8e8",
+  halo = "#909090",
+  astral = "#6d7a8c",
+  twilight = "#3b4b5f",
+
+  -- Primary accents
+  aurora = "#4caf8f",
+  nebula = "#61b8d6",
+  nova = "#9d7cd8",
+  solar = "#ff9c6a",
+  stellar = "#e4bf7f",
+
+  -- Extra accents
+  aldebaran = "#e65a5a",
+  celaeno = "#e6a2a2",
+  helios = "#80c7db",
+  io = "#b794e6",
+
+  -- Special colors
+  none = "NONE",
+  bg_dark = "#0d0d12", -- Used primarily for flojating windows
+}
+
+return colors
